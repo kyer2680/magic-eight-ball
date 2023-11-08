b3c1600aa72f394bb7e7297e355eb5d97cba9542
kyer2680 <144353210+kyer2680@users.noreply.github.com>
2023-11-08 18:36:22 +0000

    made math.random work

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
 console.log("hi");
-//when the number is 1 the ball will say yes
-var = Math.random();
-
-{if(Math.random() = 1);
-console.log(yes);
+var userName = 'Kye';
+let randomNumber = Math.round (Math.random() * 10);
+ 
+if(randomNumber === 1){
+    console.log("yes");
+}
+else if(randomNumber === 2){
+    console.log("no");
+}
+else if(randomNumber === 3){
+    console.log("It is uncertain");
+}
+else if(randomNumber === 4){
+    console.log(Absolutely)
+}
+else if(randomNumber === 5){
+    console.log("Almost Certainly");
+}
+else if(randomNumber === 6){
+    console.log("Almost certainly not");
+}
+else if(randomNumber === 7){
+    console.log("With Certainty");
+}
+else if(randomNumber === 8){
+    console.log("Unclear, try again");
+}
+else if(randomNumber === 9){
+    console.log("The magic eightball says its foretold");
+}
+else if(randomNumber === 10){
+    console.log("Its not going to happen")
 }
