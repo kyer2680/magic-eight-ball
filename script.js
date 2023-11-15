a3a2534e4a373101d5a1ac0db7bb5f5bab7bc170
kyer2680 <144353210+kyer2680@users.noreply.github.com>
2023-11-15 03:00:13 +0000

    added to script.js

--- script.js
+++ script.js
@@ -1,43 +1,55 @@
-var userName = 'Kye';
-let randomNumber = Math.round (Math.random() * 10);
- 
-if(randomNumber === 1){
-    console.log("yes");
-}
-else if(randomNumber === 2){
-    console.log("no");
-}
-else if(randomNumber === 3){
-    console.log("It is uncertain");
-}
-else if(randomNumber === 4){
-    console.log(Absolutely)
-}
-else if(randomNumber === 5){
-    console.log("Almost Certainly");
-}
-else if(randomNumber === 6){
-    console.log("Almost certainly not");
-}
-else if(randomNumber === 7){
-    console.log("With Certainty");
-}
-else if(randomNumber === 8){
-    console.log("Unclear, try again");
-}
-else if(randomNumber === 9){
-    console.log("The magic eightball says its foretold");
-}
-else{
-    console.log("Its not going to happen")
-}
+//Create an event listener
+document.getElementByID('askButton').addEventListener('click', function(){
+    let answer = ''; //Create answer variable to log answers
+    let randomIndex = Math.floor(Math.random() * 10); //Create randomIndex variable to hold rounded number
+    if(randomIndex === 0){
+        answer = 'yes';
+    } else if(randomIndex === 1){
+        answer = 'no';
+        console.log(answer);
+    } else if(randomIndex === 2){
+        answer = 'It is uncertain';
+        console.log(answer);
+    } else if(randomIndex === 3){
+        answer = 'Absolutely';
+        console.log(answer);
+    } else if(randomIndex === 4){
+        answer = 'Almost certainly';
+        console.log(answer);
+    } else if(randomIndex === 5){
+        answer = 'Almost certainly not';
+        console.log(answer);
+    } else if(randomIndex === 6){
+        answer = 'With certainty';
+        console.log(answer);
+    } else if(randomIndex === 7){
+        answer = 'Unclear try again';
+        console.log(answer);
+    } else if(randomIndex === 8){
+        answer = 'The magic eight ball says its foretold';
+        console.log(answer);
+    } else{
+        answer = 'Its not going to happen';
+        console.log(answer);
+    }
+
+    let question = document.getElementId('question').value;
+    if(question.trim() === ''){
+        alert("Please enter a question");
+    } else{
+        document.getElementId('answer').textContent = answer;
+    }
+
+});
+
+
+
+
+//Conditional statement to set answer variable to a phrase
+
+//Get value of the question element via ID
+
+//Conditional to check if you wrote a question, if so display your fortune, if not alert 
 
 
 //Check if a question is asked, trim method checks for a b
-let question = document.getElementById('question').value;
-if (question.trim() === ''){
-    alert("Please enter a question.");
-} else{
-    document.getElementById('answer').textContent = answer;
-}
-});
